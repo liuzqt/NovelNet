07c916d9f4fb5442d702564f462c07ed7489b399
davidgrey <david-grey@outlook.com>
2018-05-02 14:54:17 -0400

    refreshing

--- pysrc/test.py
+++ pysrc/test.py
@@ -18,8 +18,8 @@ from neuralcoref import Coref
 coref = Coref()
 
 text = "Stan Smith study in Pittsburgh, and he has a beautiful daughter called Annie. Smith is a teacher of Alex. Alex is a good boy and he is handsome. Bobby is a dog. The driver is stupid."
-
-with open("Harry_Potter_and_the_Sorcerers_Stone.txt", 'r') as f:
+#
+with open("littlered.txt", 'r') as f:
     text = f.read()
 
 relationship = Relationship(pipeline=coref, text=text, threshold=20, debug=True)
