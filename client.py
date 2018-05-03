8fc2ebfccb0eb5e2ed92ba732cc9101877a8941d
davidgrey <david-grey@outlook.com>
2018-05-03 01:38:28 -0400

    fix one client bug

--- client.py
+++ client.py
@@ -25,10 +25,11 @@ def runjob(joblist, machine_list):
             for m in machine_list:
                 if check_machine(m):
                     if assign(m, j):
+                        print("assign " + j +" to " + m)
                         flag = 1
                         break
             time.sleep(60)
-
+    return True
 def check_machine(machine):
     r = requests.get(machine)
     if r.content == "The server is idle":
@@ -42,4 +43,6 @@ def assign(machine, job):
         return True
 
 def main():
-    job = []
+    jobs = []
+    machines = []
+    runjob(jobs, machines)
